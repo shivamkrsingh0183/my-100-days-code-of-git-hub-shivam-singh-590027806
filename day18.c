2a94ede7f3f4cba01a0230534cf49c0a2c88a4c0
shivamkrsingh0183 <abhaykrsingh0183@gmail.com>
2025-11-19 22:52:11 +0530

    Add files via upload

--- day18.c
+++ day18.c
@@ -4,61 +4,57 @@
 
 // int main(){
 //     int n=18;
-//    // int factors=0;
+//    int factors=0;
     
 //     for(int i=1;i<=18;i++){
 //         if(n%i==0){
-//            // factors=factors+1;
+//             factors=factors+1;
 //             printf("%d \n",i);
 //         }
 
 //     }
 
 //     return 0;
-//}
+// }
     
 
 //QUESTION2:Write a program to find the HCF (GCD) of two numbers.
 
 
-// int main(){
-//     int a,b,gcd,lcm,temp;
+int main(){
+    int a,b,gcd;
+    printf("enter 1st number \n");
+    scanf("%d",&a);
+    printf("enter 2nd number \n");
+    scanf("%d",&b);
+    int remainder;
+    int remainder1;
+    if(a==1||b==1){
+        gcd=1;
+        printf("gcd of two numbers are:%d\n",gcd);
+    }
+    else{
+if(b!=0){
+    for(int i=1;i<a;i++){
+        for(int j=1;j<b;j++){
+            if(a%i==0 && b%j==0){
+                remainder=i;
+                remainder1=j;
+                if(remainder=remainder1){
+                    gcd=remainder;
+                }
+                else{
+                    gcd=1;
+                }
+            }
+        
+        }}
+        printf("gcd of a and b is :%d",gcd);
+    }
+    else{
+        printf("gcd of number cannot be find\n");
+    }
+}
 
-//     printf("enter 1st number \n");
-//     scanf("%d",&a);
-
-
-//     printf("enter 2nd number \n");
-//     scanf("%d",&b);
-
-
-// while(b!=0){
-//     temp=b;
-//     b=a%b;
-//     a=temp;
-    
-//    gcd=a;
-
-
-
-//     printf("gcd of a and b is %d \n:",gcd);
-
-
-
-
-// int lcm;
-// lcm=(a*b)/gcd;
-
-// printf("lcm of two numbers are %d:",lcm);
-
-
-// }
-
-// return 0;
-// }
-
-
-
-
-
-
+return 0;
+}
