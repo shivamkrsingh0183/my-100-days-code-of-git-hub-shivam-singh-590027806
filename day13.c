567130b9f5a1064a5a8b6067f38661b737023b02
shivamkrsingh0183 <abhaykrsingh0183@gmail.com>
2025-11-19 22:23:53 +0530

    Add files via upload

--- day13.c
+++ day13.c
@@ -1,6 +1,6 @@
 
 #include<stdio.h>
-//QUESTION2:
+//QUESTION2:write a programm to print number from 1 to n
 // int main(){
 //     int n;
 //     printf("enter n \n");
@@ -12,3 +12,34 @@
 
 //     return 0;
 // }
+
+//QUESTION1 :write  a program  to implement a basic calculator using switch case for =,-,*,/
+int main(){
+    int a;
+    scanf("%d",&a);
+    int b;
+    scanf("%d",&b);
+    int choice;
+
+    do{
+    printf("enter  your choice +/-/*or divide\n");
+    scanf("%d",&choice);
+    switch(choice){
+        case 0:printf("you result is:%d\n",a+b);break;
+        case 1:printf("you result is:%d\n",a-b);break;
+        case 2:printf("you result is:%d\n",a*b);break;
+        case 3:
+        if(b!=0){printf("you result is:%d\n",a/b);
+        }
+        else{
+            printf("divison by zero is not allowed");
+        }
+        break;
+        case 4:printf("exit");break;
+        default:printf("please enter a valid choice");
+    }
+    }while(choice!=4);
+        
+    
+    return 0;
+}
