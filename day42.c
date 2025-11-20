6edc0f5023ccc45bbaa37b0da2ffbe4016c33f36
shivamkrsingh0183 <abhaykrsingh0183@gmail.com>
2025-11-20 11:45:59 +0530

    Add files via upload

--- day42.c
+++ day42.c
@@ -1,34 +1,51 @@
 #include<stdio.h>
 #include<string.h>
 //QUESTION1:Count vowels and consonants in a string.
-// int main(){
-//     char s[]="Programminginc";
+int main(){
+    char s[]="Programminginc";
 
-//     int count1=0;
-//     int c;
+    int count1=0;
+    int c;
 
-//     int n=strlen(s);
+    int n=strlen(s);
 
-//     printf("%d\n",n);
+    printf("%d\n",n);
 
-//     for(int i=0;i<n;i++){
-//         if(s[i]=='a'|| s[i]=='A'||s[i]=='i'||s[i]=='I'||s[i]=='o'||s[i]=='O'||s[i]=='u'||s[i]=='U'||s[i]=='e'||s[i]=='E'){
-//             count1+=1;
+    for(int i=0;i<n;i++){
+        if(s[i]=='a'|| s[i]=='A'||s[i]=='i'||s[i]=='I'||s[i]=='o'||s[i]=='O'||s[i]=='u'||s[i]=='U'||s[i]=='e'||s[i]=='E'){
+            count1+=1;
         
 
-//         }
+        }
         
     
-//      }
+     }
 
-//      printf("n.o of vowels in the tring is:%d\n",count1);
+     printf("n.o of vowels in the tring is:%d\n",count1);
 
-//      c=n-count1;
+     c=n-count1;
         
-//             printf("n.o of consonant in string is :%d \n",c);
+            printf("n.o of consonant in string is :%d \n",c);
     
 
-//     return 0;
-// }
+    return 0;
+}
+//QUESTION2:Convert a lowercase string to uppercase 
+//without using built-in functions.
+int main(){
+    char s[]="string";
+   int n=strlen(s);
+    printf("length of string is:%d\n",n);
+    for(int i=0;i<n;i++){
+        if(s[i]>='a' && s[i]<='z'){
+            s[i]=(int)s[i]-32;
+        }
+        printf("%c\n",s[i]);
+        }
+        return 0;
+    }
+
+
+
 
 
